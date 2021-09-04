64c16212ca76cbf7bd97eba8f2f3fd6b58a32507
dasari97 <dasarisaikrishna97@gmail.com>
2021-09-04 15:17:50 +0530

    Updating catalogue file

--- components/redis.sh
+++ components/redis.sh
@@ -3,25 +3,26 @@
 source components/common.sh
 
 print "\e[1;33mInstalling YUM Utilitties\e[0m"
-yum install epel-release yum-utils -y
+yum install epel-release yum-utils -y &>>/tmp/log
 status_check $?
 
 print "\e[1;33mInstalling Remi Repos\e[0m;"
-yum install http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y
+yum install http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y &>>/tmp/log
 status_check $?
 
 print "\e[1;mEnabling Remi\e[0m"
-yum-config-manager --enable remi
+yum-config-manager --enable remi &>>/tmp/log
 status_check $?
 
 print "\e[1;33mInstalling Redis Component\e[0m"
-yum install redis -y
+yum install redis -y &>>/tmp/log
 status_check $?
 
 print "\e[1;33mConfiguring Redis Component\e[0m"
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf &>>/tmp/log
+status_check $?
 
-systemctl enable redis
 systemctl start redis
+systemctl enable redis &>>/tmp/log
 
 echo -e  "\e[1;35mRedis Component is ready to use.\e[0m"
