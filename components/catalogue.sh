37e4bf3afd3557e4df516a07e84a5106131b35e7
dasari97 <dasarisaikrishna97@gmail.com>
2021-09-04 14:29:02 +0530

    Updating catalogue file

--- components/catalogue.sh
+++ components/catalogue.sh
@@ -2,14 +2,14 @@
 
 source components/common.sh
 
-print "\e[1;35mInstalling Nodejs.\e[0m"
+print "\e[1;35mInstalling Nodejs.\t\t\t\e[0m"
 
 yum install nodejs make gcc-c++ -y &>>/tmp/log
 status_check $?
 
 print "\e[1;33mLet's now set up the catalogue application.\e[0m"
 
-print "\e[1;35m\nAdding new user - 'roboshop' .\e[0m"
+print "\e[1;35m\nAdding new user - 'roboshop'.\t\t\e[0m"
 id roboshop &>>/tmp/log
 if [ $? -eq 0 ];
     then
@@ -23,6 +23,7 @@ print "\e[1;35mDownloading catalogue zip file.\e[0m"
 curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>/tmp/log
 status_check $?
 
+print "\e[1;35mExtracting Catalogue.\t\e[0m"
 cd /home/roboshop
 rm -rf catalogue && unzip /tmp/catalogue.zip &>>/tmp/log && mv catalogue-main catalogue
 status_check $?
@@ -33,12 +34,12 @@ status_check $?
 
 chown roboshop:roboshop -R /home/roboshop
 
-print "\e[1;35mUpdating systemd.service file.\e[0m"
+print "\e[1;35mUpdating systemd.service file.\t\e[0m"
 sed -i -e 's/MONGO_DNSNAME/mongo.krishna.roboshop/' /home/roboshop/catalogue/systemd.service
 status_check $?
 
-print "\e[1;35mEnabling Catalogue Component.\e[0m"
+print "\e[1;35mEnabling Catalogue Component.\t\e[0m"
 mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service && systemctl daemon-reload && systemctl restart catalogue &&  systemctl enable catalogue &>>/tmp/log
 status_check $?
 
-print "\e[1;35mCatalogue Component is ready to use.\e[0m"
+print "\e[1;35mCatalogue Component is ready to use.\n\e[0m"
