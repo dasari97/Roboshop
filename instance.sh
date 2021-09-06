230a22558584c9ca5d45bc52defdcc82d6062452
dasari97 <dasarisaikrishna97@gmail.com>
2021-09-06 07:18:18 +0000

    Modifing instance file

--- instance.sh
+++ instance.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-LID='lt-02a58e5505b68e560'
+LID=lt-02a58e5505b68e560
 VLID=2
 
-aws ec2 run-instances --launch-template LaunchTemplateId=$LID,Version=$VLID
+aws ec2 run-instances --launch-template LaunchTemplateId=$LID,Version=$VLID 
