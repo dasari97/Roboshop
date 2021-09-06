b56515d91d78a4bf8b89883c5a03f3a179306932
dasari97 <dasarisaikrishna97@gmail.com>
2021-09-06 08:28:15 +0000

    Modifing instance file

--- instance.sh
+++ instance.sh
@@ -3,4 +3,30 @@
 LID="lt-02a58e5505b68e560"
 VLID=1
 
-aws ec2 run-instances --launch-template LaunchTemplateId=$LID,Version=$VLID 
+instance_name=$1
+
+if [ -z '${instance_name}' ];
+    then
+        echo -e "\e[1;33mFailed to provide Instance Name\e[0m"
+        exit 1
+        
+fi
+
+aws ec2 describe-instances --filters "Name=tag:Name,Values=Owner" | jq .Reservation.Instance[].State.name | grep running &>/dev/null
+if [ $? -eq 0 ];
+    then 
+        echo -e "\e[1;33mInstance already exists\e[0m"
+        exit 1
+fi
+
+aws ec2 describe-instances --filters "Name=tag:Name,Values=Owner" | jq .Reservation.Instance[].State.name | grep stopped &>/dev/null
+if [ $? -eq 0 ];
+    then 
+        echo -e "\e[1;33mInstance already exists\e[0m"
+        exit 1
+fi
+
+IP=$(aws ec2 run-instances --launch-template LaunchTemplateId=$LID,Version=$VLID --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=$instance_name}" "ResourceType=instances,Tags=[{Key=Name,Value=$instance_name}" | jq .Instances[].PrivateIpAddress | sed -e 's/"//g')
+
+sed -e 's/instance_name/$instance_name/' -e 's/instance_ip/$IP/' record.json >tmp/record.json
+aws route53 change-resource-record-sets --hosted-zone-id Z06986272M4T56TOB7K70 --change-batch file:///tmp/record.json
