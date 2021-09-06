08c92c1873e5d999b1209226f2ed7bfcde338b82
dasari97 <dasarisaikrishna97@gmail.com>
2021-09-06 08:49:29 +0000

    Modifing instance file

--- instance.sh
+++ instance.sh
@@ -2,28 +2,27 @@
 
 LID="lt-02a58e5505b68e560"
 VLID=1
-
 INSTANCE_NAME=$1
 
 if [ -z "${INSTANCE_NAME}" ];
     then
         echo -e "\e[1;33mFailed to provide Instance Name\e[0m"
-        exit 0
+        exit 1
         
 fi
 
-aws ec2 describe-instances --filters "Name=tag:Name,Values=$INSTANCE_NAME" | jq .Reservations[].Instance[].State.Name | grep running &>/dev/null
+aws ec2 describe-instances --filters "Name=tag:Name,Values=$INSTANCE_NAME" | jq .Reservations[].Instances[].State.Name | grep running &>/dev/null
 if [ $? -eq 0 ];
     then 
         echo -e "\e[1;33mInstance : $instances already exists\e[0m"
         exit 0
 fi
 
-aws ec2 describe-instances --filters "Name=tag:Name,Values=$INSTANCE_NAME" | jq .Reservations[].Instance[].State.Name | grep stopped &>/dev/null
+aws ec2 describe-instances --filters "Name=tag:Name,Values=$INSTANCE_NAME" | jq .Reservations[].Instances[].State.Name | grep stopped &>/dev/null
 if [ $? -eq 0 ];
     then 
         echo -e "\e[1;33mInstance : $instances already exists\e[0m"
-        exit 1
+        exit 0
 fi
 
 IP=$(aws ec2 run-instances --launch-template LaunchTemplateId=$LID,Version=$VLID --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=$INSTANCE_NAME}" "ResourceType=instances,Tags=[{Key=Name,Value=$INSTANCE_NAME}" | jq .Instances[].PrivateIpAddress | sed -e 's/"//g')
