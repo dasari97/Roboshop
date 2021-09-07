a21f64ff1eefc7c9043aa6b0032b192f7f841389
dasari97 <dasarisaikrishna97@gmail.com>
2021-09-07 09:51:59 +0000

    updated catalogue and user and common  file

--- components/common.sh
+++ components/common.sh
@@ -17,6 +17,7 @@ print() {
 
 if [ $UID -ne 0 ];
     then 
+     echo -e "\n\t\t\e[36m----------------- $1 ----------------------\e[0m\n" &>>/tmp/log
         echo -e "\e[1;31mPremission deined. Need to be a ROOT user to execute this command"
         exit 1
 fi
