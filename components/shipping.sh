45da932c24dfc80f2b2eac632df585f8450dc106
dasari97 <dasarisaikrishna97@gmail.com>
2021-09-08 07:08:39 +0000

    updated shipping file

--- components/shipping.sh
+++ components/shipping.sh
@@ -17,6 +17,5 @@ DOWNLOAD
 cd shipping &>>/tmp/log && mvn clean package &>>/tmp/log && mv target/shipping-1.0.jar shipping.jar &>>/tmp/log
 
 chown roboshop:roboshop -R /home/roboshop
-
-systemctl start shipping
+exit
 SYSTEMD_SETUP
