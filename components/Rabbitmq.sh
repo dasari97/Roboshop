21aabe5134048003485c54144f1acfaf7c8f2b9c
dasari97 <saikrishna9494342038@gmail.com>
2021-09-13 11:28:01 +0000

    updated Rabbitmq.sh file

--- components/Rabbitmq.sh
+++ components/Rabbitmq.sh
@@ -1,35 +1,32 @@
 #!/bin/bash
-if [ $UID -ne 0 ];
-    then
-        echo -e "\e[1;31mPermission denied. Need to be a root user to perform this command\e[0m"
-        exit 1
+
+source components/common.sh
+
+print "Install ErLang\t"
+  yum list installed | grep erlang &>>/tmp/log
+  if [ $? -eq 0 ]; then
+    echo "Package Already installed" &>>/tmp/log
+  else
+    yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y &>>/tmp/log
+  fi
+status_check $?
+
+Print "Setup RabbitMQ Repos"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash &>>/tmp/log
+status_check $?
+
+Print "Install RabbitMQ"
+yum install rabbitmq-server -y &>>/tmp/log
+status_check $?
+
+Print "Start RabbitMQ\t"
+systemctl enable rabbitmq-server  &>>/tmp/log  && systemctl start rabbitmq-server &>>/tmp/log
+status_check $?
+
+Print "Create App user"
+rabbitmqctl list_users | grep roboshop &>>/tmp/log
+if [ $? -ne 0 ]; then
+  rabbitmqctl add_user roboshop roboshop123 &>>/tmp/log
 fi
-
-echo -e "Setting Up RabbitMQ."
-
-echo -e "Installing Erlang.\t\t"
-yum list installed | grep erlang &>>/tmp/log
-if [ $? -eq 0 ];
-    then
-        echo -e "Erlang is already installed." &>>/tmp/log
-        status_check $?
-    else
-        yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y &>>/tmp/log
-        status_check $?
-fi
-
-   exit     
-#Setup YUM repositories for RabbitMQ.
-# curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash
-#Install RabbitMQ
-# yum install rabbitmq-server -y 
-#Start RabbitMQ
-# systemctl enable rabbitmq-server 
-# systemctl start rabbitmq-server 
-
-
-#Create application user
-# rabbitmqctl add_user roboshop roboshop123
-# rabbitmqctl set_user_tags roboshop administrator
-# rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
-echo -e "RabbitMq setup is finished."
+rabbitmqctl set_user_tags roboshop administrator &>>/tmp/log && rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>/tmp/log
+status_check $?
