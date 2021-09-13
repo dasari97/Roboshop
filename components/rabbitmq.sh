c43615649985d5dc4f38223b4d0f90201a4b1477
dasari97 <saikrishna9494342038@gmail.com>
2021-09-13 07:29:52 +0000

    uploading rabbitmq file

--- components/rabbitmq.sh
+++ components/rabbitmq.sh
@@ -1,25 +1,67 @@
 #!/bin/bash
+echo -e "Setting Up Rabbitmq component."
+
+echo -e  "install Erlang.\t\t"
+yum list installed | grep erlang
+if [ $? -eq 0 ];
+    then
+        echo -e "Erlang is already installed" &>>/tmp/log
+        if [ $1 -eq 0 ];
+        then
+            echo -e "\e[1;32mSUCCESS\e[0m"
+        else
+            echo -e "\e[1;31mFAILURE\e[0m"
+            exit 2
+    fi
+    else
+        yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y &>>/tmp/log
+    if [ $1 -eq 0 ];
+        then
+            echo -e "\e[1;32mSUCCESS\e[0m"
+        else
+            echo -e "\e[1;31mFAILURE\e[0m"
+            exit 2
+    fi
+fi
 
 
-source components/common.sh
-
-print "install Erlang.\t\t"
-yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y &>>/tmp/log
-status_check $?
-
-print "Setup YUM repos for RabbitMQ.\t"
+echo -e "Setup YUM repos for RabbitMQ.\t"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash &>>/tmp/log
-status_check $?
+if [ $1 -eq 0 ];
+        then
+            echo -e "\e[1;32mSUCCESS\e[0m"
+        else
+            echo -e "\e[1;31mFAILURE\e[0m"
+            exit 2
+    fi
 
-print "Install RabbitMQ.\t\t"
+echo -e "Install RabbitMQ.\t\t"
 yum install rabbitmq-server -y &>>/tmp/log
-status_check $?
+if [ $1 -eq 0 ];
+        then
+            echo -e "\e[1;32mSUCCESS\e[0m"
+        else
+            echo -e "\e[1;31mFAILURE\e[0m"
+            exit 2
+    fi
 
-print "Start RabbitMQ.\t"
+echo -e  "Start RabbitMQ.\t"
 systemctl enable rabbitmq-server &>>/tmp/log && systemctl start rabbitmq-server &>>/tmp/log
-status_check $?
+if [ $1 -eq 0 ];
+        then
+            echo -e "\e[1;32mSUCCESS\e[0m"
+        else
+            echo -e "\e[1;31mFAILURE\e[0m"
+            exit 2
+    fi
 
 
-print "Creating application user.\t"
+echo -e "Creating application user.\t"
 rabbitmqctl add_user roboshop roboshop123 &>>/tmp/log && rabbitmqctl set_user_tags roboshop administrator &>>/tmp/log && rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>/tmp/log
-status_check $?
+if [ $1 -eq 0 ];
+        then
+            echo -e "\e[1;32mSUCCESS\e[0m"
+        else
+            echo -e "\e[1;31mFAILURE\e[0m"
+            exit 2
+    fi
