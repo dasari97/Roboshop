ed3785ad0001dc904a68f583a696e22fd381be6a
dasari97 <saikrishna9494342038@gmail.com>
2021-09-13 16:42:30 +0000

    updated Rabbitmq.sh file

--- components/rabbitmq.sh
+++ components/rabbitmq.sh
@@ -1,36 +1,33 @@
 #!/bin/bash
 
-source components/commom.sh
+source components/common.sh
 
-print "Install Erlang\t"
-    yum list installed | grep erlang &>>/tmp/log
-    if [ $? -eq 0 ];
-        then
-            echo "Package Already Installed" &>>/tmp/log
-        else
-            yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.e17.x86_64.rpm -y &>>/tmp/log
-    fi
+print "Install ErLang\t"
+  yum list installed | grep erlang &>>/tmp/log
+  if [ $? -eq 0 ]; then
+    echo "Package Already installed" &>>/tmp/log
+  else
+    yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y &>>/tmp/log
+  fi
 status_check $?
 
-print "setting Rabbitmq Repos\t"
+echo -e "Setting RabbitMQ Repos"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash &>>/tmp/log
 status_check $?
 
-print "Install RabbitMQ.\t"
+
+echo -e "Install RabbitMQ"
 yum install rabbitmq-server -y &>>/tmp/log
 status_check $?
 
-print "Starting RabbitMQ.\t"
-systemctl enable rabbitmq-server &>>/tmp/log && systemctl start rabbitmq-server &>>/tmp/log
+echo -e "Start RabbitMQ\t"
+systemctl enable rabbitmq-server  &>>/tmp/log  && systemctl start rabbitmq-server &>>/tmp/log
 status_check $?
 
-print "creating App user\t"
+echo -e "Create App user"
 rabbitmqctl list_users | grep roboshop &>>/tmp/log
-if [ $? -ne 0 ];
-    then
-        rabbitmqctl add_user roboshop roboshop123 &>>/tmp/log
+if [ $? -ne 0 ]; then
+  rabbitmqctl add_user roboshop roboshop123 &>>/tmp/log
 fi
-
 rabbitmqctl set_user_tags roboshop administrator &>>/tmp/log && rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>/tmp/log
 status_check $?
-        
