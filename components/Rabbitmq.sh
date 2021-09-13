3f2cb701f90dece9afb44fef28f50f7ac5c59bb2
dasari97 <saikrishna9494342038@gmail.com>
2021-09-13 08:50:02 +0000

    updated Rabbitmq.sh file

--- components/Rabbitmq.sh
+++ components/Rabbitmq.sh
@@ -2,7 +2,7 @@
 
 if [ $UID -ne 0 ];
     then 
-            echo -e "\e[1;31mPremission deined. Need to be a ROOT user to execute this command"
+            echo -e "\e[1;31mPremission deined. Need to be a ROOT user to execute this command\e[0m"
         exit 1
 fi
 
